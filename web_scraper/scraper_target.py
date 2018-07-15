5206b74feddbb3b426c4c2b318da39e3fee4aa3a
mzorman <39628182+mzorman@users.noreply.github.com>
2018-07-15 13:08:43 -0500

    delete

--- web_scraper/scraper_target.py
+++
@@ -1,7 +0,0 @@
-target = {
-'Playboi-carti':['magnolia', 'shoota', 'lookin', 'wokeuplikethis', 'let-it-go', 'half-and-half',
-'new-choppa', 'other-shit', 'no-9', 'dothatshit', 'lame-niggaz', 'yah-mean', 'flex', 'kelly-k', 'had-2', 'long-time-intro', 'rip',
-'lean-4-real', 'old-money', 'love-hurts','shoota', 'right-now', 'poke-it-out', 'home-kod', 'fell-in-luv', 'foreign', 'pull-up',
-'mileage', 'flatbed-freestyle', 'no-time', 'middle-of-the-summer', 'choppa-wont-miss',
-'rip-fredo-notice-me', 'top']
-}
